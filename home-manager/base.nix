09a5615ae9b3ef06da92ba9720b006c484be6618
Karl Skewes <karl.skewes@gmail.com>
2021-10-08 20:25:31 +1300

    Add readline / .inputrc

--- home-manager/base.nix
+++ home-manager/base.nix
@@ -177,6 +177,38 @@
     # vimAlias = true; # bash alias to LunarVim lvim instead
   };
 
+  programs.readline = {
+    enable = true;
+    extraConfig = ''
+      # Make Tab autocomplete regardless of filename case
+      set completion-ignore-case on
+
+      # List all matches in case multiple possible completions are possible
+      set show-all-if-ambiguous on
+
+      # Immediately add a trailing slash when autocompleting symlinks to directories
+      set mark-symlinked-directories on
+
+      # Use the text that has already been typed as the prefix for searching through
+      # commands (i.e. more intelligent Up/Down behavior)
+      "\e[B": history-search-forward
+      "\e[A": history-search-backward
+
+      # Do not autocomplete hidden files unless the pattern explicitly begins with a dot
+      set match-hidden-files off
+
+      # Show all autocomplete results at once
+      set page-completions off
+
+      # Be more intelligent when autocompleting by also looking at the text after
+      # the cursor. For example, when the current line is "cd ~/src/mozil", and
+      # the cursor is on the "z", pressing Tab will not autocomplete it to "cd
+      # ~/src/mozillail", but to "cd ~/src/mozilla". (This is supported by the
+      # Readline used by Bash 4.)
+      set skip-completed-text on
+    '';
+  };
+
   programs.tmux = {
     enable = true;
     baseIndex = 1;
