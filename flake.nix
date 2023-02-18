d40ae3f3045dc6eec61e0f69114d4862902bdee8
Karl Skewes <karl.skewes@gmail.com>
2023-02-18 14:56:59 +1300

    update - unpin for neovim, fixed upstream

--- flake.nix
+++ flake.nix
@@ -12,8 +12,8 @@
     neovim-nightly-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
       # Pin to a nixpkgs revision that doesn't have NixOS/nixpkgs#208103 yet
-      inputs.nixpkgs.url =
-        "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+      # inputs.nixpkgs.url =
+      # "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
     };
 
     nix-extra.url = "path:/home/karl/src/nix-extra";
