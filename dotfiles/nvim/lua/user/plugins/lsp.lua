d648a78e88a2d57693a0168e6f22c33554d53837
Karl Skewes <hello@karlskewes.com>
2024-03-01 21:19:39 +1300

    fix: ctrl+y to accept completion, get out of <CR>

--- dotfiles/nvim/lua/user/plugins/lsp.lua
+++ dotfiles/nvim/lua/user/plugins/lsp.lua
@@ -286,7 +286,7 @@ return {
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm {
+                    ['<C-y>'] = cmp.mapping.confirm {
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true
                     },
