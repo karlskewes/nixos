fbac5ecf80ad40160bbc75d5c83d5b50cb04eec8
Karl Skewes <hello@karlskewes.com>
2024-05-25 08:47:06 +1200

    feat: neovim git and diff plugins
    github.com/lewis6991/gitsigns.nvim:
    Great plugin but the diff view opens HEAD on a separate pane so closing
    the separate pane requires navigation across rather than `:q` (keymap).
    Issue #585.
    Have had plugin installed for a long time and haven't really been using.
    
    `:Telescope git_status` and other Telescope `<leader>sv` `vcs` keymaps
    provide access to split panes for all files.
    
    Ultimately only want:
    1. signs in gutter, mini.diff
    2. git add|reset file(s), mini.git
    3. git status browse, Telescope

--- dotfiles/nvim/lua/user/plugins/plugins.lua
+++ dotfiles/nvim/lua/user/plugins/plugins.lua
@@ -2,7 +2,36 @@
 return {
     {
         -- :Git commands.
-        'tpope/vim-fugitive'
+        'echasnovski/mini-git',
+        version = false,
+        main = 'mini.git',
+        config = function()
+            require('mini.git').setup()
+
+            vim.keymap.set("n", "<leader>ga", "<CMD>Git add %<CR>",
+                           {desc = "[G]it [a]dd buffer"})
+            vim.keymap.set("n", "<leader>gA", "<CMD>Git add -A<CR>",
+                           {desc = "[G]it [A]dd all files"})
+            vim.keymap.set("n", "<leader>gd", "<CMD>Git diff %<CR>",
+                           {desc = "[G]it [d]iff buffer"})
+            vim.keymap.set("n", "<leader>gD", "<CMD>Git diff<CR>",
+                           {desc = "[G]it [D]iff all files"})
+            vim.keymap.set("n", "<leader>gr", "<CMD>Git reset %<CR>",
+                           {desc = "[G]it [r]eset buffer"})
+            vim.keymap.set("n", "<leader>gs", "<CMD>Git status<CR>",
+                           {desc = "[G]it [s]tatus"})
+        end
+    }, {
+        'echasnovski/mini.diff',
+        version = false,
+        config = function()
+            require('mini.diff').setup({
+                view = {
+                    style = 'sign',
+                    signs = {add = '+', change = '~', delete = '-'}
+                }
+            })
+        end
     }, {
         -- Detect tabstop and shiftwidth automatically.
         'tpope/vim-sleuth'
@@ -64,78 +93,6 @@ return {
                 require("trouble").toggle("lsp_references")
             end, {desc = "[T]rouble LSP References"})
         end
-    }, {
-        -- Git decorations, hunk and other utilities.
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            -- See `:help gitsigns.txt`
-            signs = {
-                add = {text = '+'},
-                change = {text = '~'},
-                delete = {text = '_'},
-                topdelete = {text = '‾'},
-                changedelete = {text = '~'}
-            },
-            on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
-
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-
-                -- Navigation
-                map({'n', 'v'}, ']c', function()
-                    if vim.wo.diff then return ']c' end
-                    vim.schedule(function() gs.next_hunk() end)
-                    return '<Ignore>'
-                end, {expr = true, desc = 'Jump to next hunk'})
-
-                map({'n', 'v'}, '[c', function()
-                    if vim.wo.diff then return '[c' end
-                    vim.schedule(function() gs.prev_hunk() end)
-                    return '<Ignore>'
-                end, {expr = true, desc = 'Jump to previous hunk'})
-
-                -- Actions
-                -- visual mode
-                map('v', '<leader>gs', function()
-                    gs.stage_hunk {vim.fn.line '.', vim.fn.line 'v'}
-                end, {desc = 'stage git hunk'})
-                map('v', '<leader>gr', function()
-                    gs.reset_hunk {vim.fn.line '.', vim.fn.line 'v'}
-                end, {desc = 'reset git hunk'})
-                -- normal mode
-                map('n', '<leader>gs', gs.stage_hunk, {desc = 'git stage hunk'})
-                map('n', '<leader>gr', gs.reset_hunk, {desc = 'git reset hunk'})
-                map('n', '<leader>gS', gs.stage_buffer,
-                    {desc = 'git Stage buffer'})
-                map('n', '<leader>gu', gs.undo_stage_hunk,
-                    {desc = 'undo stage hunk'})
-                map('n', '<leader>gR', gs.reset_buffer,
-                    {desc = 'git Reset buffer'})
-                map('n', '<leader>gp', gs.preview_hunk,
-                    {desc = 'preview git hunk'})
-                map('n', '<leader>gb',
-                    function() gs.blame_line {full = false} end,
-                    {desc = 'git blame line'})
-                map('n', '<leader>gd', gs.diffthis,
-                    {desc = 'git diff against index'})
-                map('n', '<leader>gD', function() gs.diffthis '~' end,
-                    {desc = 'git diff against last commit'})
-
-                -- Toggles
-                map('n', '<leader>gy', gs.toggle_current_line_blame,
-                    {desc = 'toggle git blame line'})
-                map('n', '<leader>gz', gs.toggle_deleted,
-                    {desc = 'toggle git show deleted'})
-
-                -- Text object
-                map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>',
-                    {desc = 'select git hunk'})
-            end
-        }
     }, {
         -- remember last place in file
         "ethanholz/nvim-lastplace",
