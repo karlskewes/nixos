d7f61c15ada38a719f6b265e0e3d13fdbc3ef49c
Karl Skewes <hello@karlskewes.com>
2024-06-13 08:05:21 +1200

    chore: Update bat catppuccin theme

--- home-manager/shared.nix
+++ home-manager/shared.nix
@@ -170,13 +170,13 @@ in
       catppuccin = {
         src = pkgs.fetchFromGitHub {
           owner = "catppuccin";
-          repo = "bat"; # Bat uses sublime syntax for its themes
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+          repo = "bat";
+          rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
           # nix-shell -p nix-prefetch
-          # nix-prefetch fetchFromGitHub --owner catppuccin --repo bat --rev ba4d16880d63e656acced2b7d4e034e4a93f74b1
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          # nix-prefetch --option extra-experimental-features flakes fetchFromGitHub --owner catppuccin --repo bat --rev d714cc1d358ea51bfc02550dabab693f70cccea0
+          sha256 = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
         };
-        file = "Catppuccin-macchiato.tmTheme";
+        file = "themes/Catppuccin Mocha.tmTheme";
       };
     };
   };
