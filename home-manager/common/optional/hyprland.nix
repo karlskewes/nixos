8d941603ad82a26e66593611f69154aac72814dc
Karl Skewes <hello@karlskewes.com>
2024-12-13 07:48:31 +1000

    fix: wayland waybar pulseaudio icons and spacing

--- home-manager/common/optional/hyprland.nix
+++ home-manager/common/optional/hyprland.nix
@@ -111,16 +111,14 @@
         };
         "pulseaudio" = {
           # // "scroll-step" = 1; // %, can be a float
-          "format" = "{volume}% {icon} {format_source}";
-          "format-bluetooth" = "{volume}% {icon} {format_source}";
-          "format-bluetooth-muted" = " {icon} {format_source}";
+          "format" = "{volume}% {icon}  {format_source}";
+          "format-bluetooth" = "{volume}% {icon}  {format_source}";
+          "format-bluetooth-muted" = " {icon}  {format_source}";
           "format-muted" = " {format_source}";
           "format-source" = "{volume}% ";
           "format-source-muted" = "";
           "format-icons" = {
             "headphone" = "";
-            "hands-free" = "";
-            "headset" = "";
             "phone" = "";
             "portable" = "";
             "car" = "";
