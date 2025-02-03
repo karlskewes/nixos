0c9302c4de38b83e68c98494606ae76f44aa1b9c
Karl Skewes <hello@karlskewes.com>
2025-02-03 20:12:05 +1000

    feat: neovim drop symbols from statusline, blocks filename

--- dotfiles/nvim/lua/plugins.lua
+++ dotfiles/nvim/lua/plugins.lua
@@ -171,20 +171,4 @@ local lualine_opts = {
   },
 }
 
-local trouble = require('trouble')
-local symbols = trouble.statusline({
-  mode = 'lsp_document_symbols',
-  groups = {},
-  title = false,
-  filter = { range = true },
-  format = '{kind_icon}{symbol.name:Normal}',
-  -- The following line is needed to fix the background color
-  -- Set it to the lualine section you want to use
-  hl_group = 'lualine_c_normal',
-})
-table.insert(lualine_opts.sections.lualine_c, {
-  symbols.get,
-  cond = symbols.has,
-})
-
 require('lualine').setup(lualine_opts)
