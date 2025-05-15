8375be80a20c1c047c4c1ef573f90562642434a4
Karl Skewes <hello@karlskewes.com>
2025-05-15 18:45:28 +1000

    fix: cosmic desktop gnome keyring autostart

--- home-manager/common/optional/cosmic.nix
+++ home-manager/common/optional/cosmic.nix
@@ -10,12 +10,6 @@
     size = lib.mkDefault 24;
   };
 
-  programs.bash.shellAliases = {
-    # Copy Paste to clipboard.
-    pbcopy = "wl-copy";
-    pbpaste = "wl-paste";
-  };
-
   home.packages = with pkgs; [
     wl-clipboard
 
@@ -26,23 +20,17 @@
     xdg-desktop-portal-gtk
   ];
 
+  programs.bash.shellAliases = {
+    # Copy Paste to clipboard.
+    pbcopy = "wl-copy";
+    pbpaste = "wl-paste";
+  };
+
+  services.gnome-keyring = {
+    enable = true;
+    components = [ "pkcs11" "secrets" "ssh" ];
+  };
+
   xdg.portal.extraPortals =
     [ pkgs.xdg-desktop-portal-cosmic pkgs.xdg-desktop-portal-gtk ];
-  # Must start after keyring to be effective...
-  # xdg.configFile."autostart/gnome-keyring-daemon.desktop".text = ''
-  #   [Desktop Entry]
-  #   Version=1.0
-  #   Name=gnome-keyring-daemon
-  #   GenericName=Gnome Keyring Daemon
-  #   Comment=Gnome Keyring Daemon
-  #   Exec=/run/wrappers/bin/gnome-keyring-daemon --start --daemonize
-  #   Icon=gnome
-  #   Terminal=false
-  #   Type=Application
-  #   Categories=Utility;
-  #   StartupNotify=true
-  #   Hidden=true
-  #   X-GNOME-Autostart-enabled=true
-  # '';
-
 }
