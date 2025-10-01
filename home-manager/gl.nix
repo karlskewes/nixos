bf6e9b4e1688dc535b72245520415e951afddf49
Karl Skewes <hello@karlskewes.com>
2025-10-01 13:52:24 +1000

    feat: gcloud cli and more tools

--- home-manager/gl.nix
+++ home-manager/gl.nix
@@ -1,56 +1,63 @@
-({ lib, pkgs, isDarwin, isLinux, ... }: {
-  imports = [
-    ./user-karl.nix
+({ lib, pkgs, isDarwin, isLinux, ... }:
+  let
+    gdk = pkgs.google-cloud-sdk.withExtraComponents
+      (with pkgs.google-cloud-sdk.components; [ gke-gcloud-auth-plugin ]);
 
-    ./common/global
+  in {
+    imports = [
+      ./user-karl.nix
+      ./common/global
 
-    ./common/optional/dev.nix
-  ] # #
-    ++ (lib.optionals isDarwin [
-      ./common/optional/desktop.nix
+      ./common/optional/dev.nix
+    ] # #
+      ++ (lib.optionals isDarwin [
+        ./common/optional/desktop.nix
+        # #
+      ])
       # #
-    ])
-    # #
-    ++ (lib.optionals isLinux [
-      ./common/optional/cosmic.nix
-      # #
-    ]);
+      ++ (lib.optionals isLinux [
+        ./common/optional/cosmic.nix
+        # #
+      ]);
 
-  common.git.signing = { enable = true; };
+    common.git.signing = { enable = true; };
 
-  desktop.firefox = { enable = true; };
+    desktop.firefox = { enable = true; };
 
-  programs.bash.shellAliases = {
-    dco = "docker-compose";
-    k = "kubectl";
-    # podman docker host export
-    # https://podman-desktop.io/docs/migrating-from-docker/using-the-docker_host-environment-variable
-    pdh =
-      "export DOCKER_HOST=unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')";
-  };
+    programs.bash.shellAliases = {
+      docker = lib.mkIf (isDarwin) "podman";
+      dco = "docker-compose";
+      k = "kubectl";
+      # podman docker host export
+      # https://podman-desktop.io/docs/migrating-from-docker/using-the-docker_host-environment-variable
+      pdh =
+        "export DOCKER_HOST=unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')";
+    };
 
-  home.packages = with pkgs;
-    [ ] ++ (lib.optionals isDarwin [
-      google-chrome # chromium variants not supported on darwin
-      slack
+    home.packages = with pkgs;
+      [ ] ++ (lib.optionals isDarwin [
+        google-chrome # chromium variants not supported on darwin
+        slack
 
-      awscli2
-      google-cloud-sdk
-      azure-cli
+        awscli2
+        gdk
+        azure-cli
 
-      docker-buildx
-      podman # docker replacement
-      podman-compose
-      kind
-      kubernetes-helm
+        docker-buildx
+        podman # docker replacement
+        podman-compose
+        kind
+        kubernetes-helm
 
-      jsonnet-bundler
-      tanka
-      yq
-    ]) ++ (lib.optionals isLinux [
-      asahi-bless
-      asahi-btsync
-      asahi-nvram
-      asahi-wifisync
-    ]);
-})
+        gh
+        pre-commit
+        jsonnet-bundler
+        tanka
+        yq
+      ]) ++ (lib.optionals isLinux [
+        asahi-bless
+        asahi-btsync
+        asahi-nvram
+        asahi-wifisync
+      ]);
+  })
