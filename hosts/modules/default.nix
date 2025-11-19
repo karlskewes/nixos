eb2287e859393ca4b3771e251c97214f7e45a9b5
Karl Skewes <hello@karlskewes.com>
2025-11-20 05:13:48 +1000

    feat: add nixos apple silicon cachix
    Reduce the amount of kernel compilations.

--- hosts/modules/default.nix
+++ hosts/modules/default.nix
@@ -117,11 +117,15 @@
       trusted-users =
         [ "karl" "karlskewes" ]; # ability to add cache substituters.
 
-      substituters =
-        [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://nixos-apple-silicon.cachix.org"
+      ];
       trusted-public-keys = [
         # cache.nixos.org key built-in
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       ];
     };
 
