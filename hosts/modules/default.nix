47e2ff1bf3e932dbb5f4e5eaee0f7be3084523e1
Karl Skewes <hello@karlskewes.com>
2025-11-20 05:21:24 +1000

    feat: remove nix-community and nas cachix
    https://github.com/nix-community/nixos-apple-silicon/blob/main/docs/binary-cache.md#who-are-you-trusting-by-adding-this-binary-cache
    
    https://garnix.io/blog/stop-trusting-nix-caches

--- hosts/modules/default.nix
+++ hosts/modules/default.nix
@@ -119,13 +119,17 @@
 
       substituters = [
         "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://nixos-apple-silicon.cachix.org"
+
+        # https://garnix.io/blog/stop-trusting-nix-caches
+        # "https://nix-community.cachix.org"
+        # "https://nixos-apple-silicon.cachix.org"
       ];
       trusted-public-keys = [
         # cache.nixos.org key built-in
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+
+        # https://garnix.io/blog/stop-trusting-nix-caches
+        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       ];
     };
 
