7f0b54bea19bded1580402873fa295ea531b9a29
Karl Skewes <hello@karlskewes.com>
2026-06-26 08:29:47 +1000

    chore: update nixpkgs

--- hosts/modules/cosmic.nix
+++ hosts/modules/cosmic.nix
@@ -5,9 +5,8 @@
 
   # xdg defaults not working.
   # https://github.com/lilyinstarlight/nixos-cosmic/issues/273
-  systemd.user.extraConfig = ''
-    DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-  '';
+  systemd.user.settings.Manager.DefaultEnvironment =
+    "PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin";
 
   # enable clipboard management: zwlr_data_control_manager_v1
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
